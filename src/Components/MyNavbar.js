31380b98e7b63a7b6fd61336acda8e811a9fe0e2
Riwa <riwa.karam@gmail.com>
2024-11-30 22:48:33 -0800

    added favicon, fixed the navbar, added pic and bio

--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom"; // Import NavLink from react-router-dom
 
 const MyNavbar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 {/* Left side - Name */}
-                <Navbar.Brand href="#">Riwa Karam</Navbar.Brand>
+                <Navbar.Brand>Riwa Karam</Navbar.Brand>
 
                 {/* Centered nav items */}
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <div className="centered-nav">
-                        <Nav>
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#experience">Experience</Nav.Link>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Nav className="ml-auto">
+                            <Nav.Link as={NavLink} to="/" exact>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link as={NavLink} to="/about">
+                                About
+                            </Nav.Link>
+                            <Nav.Link as={NavLink} to="/experience">
+                                Experience
+                            </Nav.Link>
+                            <Nav.Link as={NavLink} to="/contact">
+                                Contact
+                            </Nav.Link>
                         </Nav>
                     </div>
                 </Navbar.Collapse>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,45 @@
 import React from "react";
-import Card from "../Components/Card";
+import photo from "../Images/Photo Passeport.JPG";
 
-const Home = () => (
-    <div className="container text-center mt-5">
-        <h1>Riwa Karam</h1>
-        <p className="mt-3">
-            PhD Student at the University of California, Irvine
-        </p>
-        <div className="row mt-5">
-            <div className="col-md-4">
-                <Card title="Feature 1" text="Explore the first feature." />
+const Home = () => {
+    return (
+        <div className="container home-container">
+            {/* Header Section */}
+            <div className="row justify-content-center text-center mb-4">
+                <h1 className="col-12 name">Riwa Karam</h1>
+                <p className="col-12 description">
+                    PhD Student at the University of California Irvine
+                </p>
             </div>
-            <div className="col-md-4">
-                <Card title="Feature 2" text="Discover the second feature." />
-            </div>
-            <div className="col-md-4">
-                <Card title="Feature 3" text="Dive into the third feature." />
+
+            {/* Main Content Section */}
+            <div className="row justify-content-center align-items-center">
+                {/* Image Section */}
+                <div className="col-12 col-lg-6 image-section mb-4 mb-lg-0 d-flex justify-content-center">
+                    <img
+                        src={photo} // Replace with your image path
+                        alt="Riwa Karam"
+                        className="profile-image img-fluid"
+                    />
+                </div>
+
+                {/* Bio Section */}
+                <div className="col-12 col-lg-6 bio-section text-center text-lg-left">
+                    <h2>Bio</h2>
+                    <p>
+                        I am currently a PhD student in Robotics and Controls at
+                        the University of California, Irvine. My research
+                        interests include machine learning, robotics, and
+                        control systems. I have previously worked on projects
+                        involving piezo-electric actuators and Gaussian Process
+                        Regression for force estimation. I am passionate about
+                        exploring innovative solutions in mechatronics and
+                        robotics.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Home;
