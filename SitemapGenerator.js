de591c792b91d13d17636de3715a6ef5cecaaa75
Riwa <riwa.karam@gmail.com>
2025-03-11 15:44:49 -0700

    edits

--- SitemapGenerator.js
+++ SitemapGenerator.js
@@ -1,6 +1,3 @@
-// SitemapGenerator.js
-
-// If generating sitemap, ignore asset files (images, PDFs, etc.)
 if (process.env.GENERATE_SITEMAP === "true") {
     const extensionsToIgnore = [
         ".jpg",
@@ -11,23 +8,37 @@ if (process.env.GENERATE_SITEMAP === "true") {
         ".ico",
     ];
     extensionsToIgnore.forEach((ext) => {
-        // Also ignore uppercase variants
         require.extensions[ext] = () => {};
         require.extensions[ext.toUpperCase()] = () => {};
     });
 }
 
-// Now load Babel Register to transpile JSX
 require("@babel/register")({
     presets: ["@babel/preset-env", "@babel/preset-react"],
     extensions: [".js", ".jsx"],
     ignore: [/node_modules/],
 });
 
-// Now import your app/router after the asset hooks are set
 const router = require("./src/App").default;
 const Sitemap = require("react-router-sitemap").default;
 
-new Sitemap(router)
-    .build("https://riwakaram.github.io") // Replace with your actual URL
-    .save("./public/sitemap.xml");
+const sitemapInstance = new Sitemap(router).build(
+    "https://riwakaram.github.io"
+);
+
+if (sitemapInstance && sitemapInstance.urls) {
+    sitemapInstance.urls.push({
+        loc: "https://riwakaram.github.io/about",
+    });
+    sitemapInstance.urls.push({
+        loc: "https://riwakaram.github.io/experience",
+    });
+    sitemapInstance.urls.push({
+        loc: "https://riwakaram.github.io/publications",
+    });
+    sitemapInstance.urls.push({
+        loc: "https://riwakaram.github.io/contact",
+    });
+}
+
+sitemapInstance.save("./public/sitemap.xml");
