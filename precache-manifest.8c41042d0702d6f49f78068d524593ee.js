58c1f8f91ec3a0eb8c9dca018d50f5634905f210
Riwa Karam <riwa.karam@gmail.com>
2025-04-28 19:45:39 -0700

    Updates

---
+++ precache-manifest.8c41042d0702d6f49f78068d524593ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10678ae6d5198d0759b1f44a07dd1f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc1ca5ff421b15f1f667",
+    "url": "/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "b22d5aee2e989933c3fe",
+    "url": "/static/css/main.7ace8043.chunk.css"
+  },
+  {
+    "revision": "cc1ca5ff421b15f1f667",
+    "url": "/static/js/2.9b264c7b.chunk.js"
+  },
+  {
+    "revision": "f293f9ea764746df48657a85f02822ca",
+    "url": "/static/js/2.9b264c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2730bfe862ed42b14c6",
+    "url": "/static/js/3.606a8692.chunk.js"
+  },
+  {
+    "revision": "b22d5aee2e989933c3fe",
+    "url": "/static/js/main.a63785ba.chunk.js"
+  },
+  {
+    "revision": "0af6d2f143b846b89a40",
+    "url": "/static/js/runtime-main.10c2d2cb.js"
+  },
+  {
+    "revision": "df7297dc49171bd233ed73615ab39848",
+    "url": "/static/media/Profile Pic.df7297dc.jpg"
+  },
+  {
+    "revision": "f71f52a507df3c16a8f2f301215ab04b",
+    "url": "/static/media/RiwaKaram_Resume.f71f52a5.pdf"
+  },
+  {
+    "revision": "49bcf5d663ec3791bd2e9af750f7832b",
+    "url": "/static/media/favicon.49bcf5d6.ico"
+  }
+]);
