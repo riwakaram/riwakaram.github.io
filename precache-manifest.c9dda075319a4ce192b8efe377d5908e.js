84803069343d7d9517551f2ac6aafe16bb654f50
Riwa <riwa.karam@gmail.com>
2025-05-03 13:15:31 -0700

    Updates

---
+++ precache-manifest.c9dda075319a4ce192b8efe377d5908e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3b4c0eca2d11f785c5fd298c16f903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1149a6c1384ecce355c",
+    "url": "/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "5575f4392afcbd51ffb9",
+    "url": "/static/css/main.70da4ebc.chunk.css"
+  },
+  {
+    "revision": "a1149a6c1384ecce355c",
+    "url": "/static/js/2.ca00ff12.chunk.js"
+  },
+  {
+    "revision": "f293f9ea764746df48657a85f02822ca",
+    "url": "/static/js/2.ca00ff12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312798beea233dce9c6d",
+    "url": "/static/js/3.f0a313fb.chunk.js"
+  },
+  {
+    "revision": "5575f4392afcbd51ffb9",
+    "url": "/static/js/main.823b6656.chunk.js"
+  },
+  {
+    "revision": "04ebc92b98451cc66568",
+    "url": "/static/js/runtime-main.0b5c24bd.js"
+  },
+  {
+    "revision": "9799093fee62d4c7b300107e9256c88d",
+    "url": "/static/media/ASML Goodies.9799093f.jpeg"
+  },
+  {
+    "revision": "09ba2126dbb2b6de342f7469c41072e2",
+    "url": "/static/media/ASML ID.09ba2126.jpg"
+  },
+  {
+    "revision": "4c24403399d0cf27dfe6a74a45a1f454",
+    "url": "/static/media/ASMLCard.4c244033.jpeg"
+  },
+  {
+    "revision": "a7c69605086ac6a044b3440552c0cf28",
+    "url": "/static/media/ASMLPosterAll.a7c69605.jpeg"
+  },
+  {
+    "revision": "8ce6bad055e4d52f1a0a818b11c76548",
+    "url": "/static/media/Prelim.8ce6bad0.pdf"
+  },
+  {
+    "revision": "df7297dc49171bd233ed73615ab39848",
+    "url": "/static/media/Profile Pic.df7297dc.jpg"
+  },
+  {
+    "revision": "f71f52a507df3c16a8f2f301215ab04b",
+    "url": "/static/media/RiwaKaram_Resume.f71f52a5.pdf"
+  },
+  {
+    "revision": "140ef8113b7a9b611dee744ce99ab394",
+    "url": "/static/media/SSCW2025Dinner.140ef811.JPG"
+  },
+  {
+    "revision": "973c9136edacc8d2f2e1517f390e877e",
+    "url": "/static/media/SSCWGroup2025.973c9136.jpg"
+  },
+  {
+    "revision": "ff5b4c1821cfd44435c2384d2f4db7f4",
+    "url": "/static/media/SSCWMe_1_2025.ff5b4c18.jpg"
+  },
+  {
+    "revision": "0a9a514daf9c4c487e5f6e1a65b4bcc4",
+    "url": "/static/media/SSCWMe_2_2025.0a9a514d.jpg"
+  },
+  {
+    "revision": "49bcf5d663ec3791bd2e9af750f7832b",
+    "url": "/static/media/favicon.49bcf5d6.ico"
+  }
+]);
