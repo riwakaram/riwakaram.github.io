98bcb1d3eae0b7e4f5613c1d281323c5932d1428
Riwa <riwa.karam@gmail.com>
2025-06-09 15:23:43 -0700

    Updates

---
+++ precache-manifest.00ae17aa0aa8f6ac270007120fa6ce4c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e79140d639c7159c0a6b0768a2e11c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55ea02f2f0cead79c93",
+    "url": "/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "68e458fb5b5771285523",
+    "url": "/static/css/main.70da4ebc.chunk.css"
+  },
+  {
+    "revision": "c55ea02f2f0cead79c93",
+    "url": "/static/js/2.6aca807f.chunk.js"
+  },
+  {
+    "revision": "f293f9ea764746df48657a85f02822ca",
+    "url": "/static/js/2.6aca807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb24e1f3821f298ca1c",
+    "url": "/static/js/3.acaa8d23.chunk.js"
+  },
+  {
+    "revision": "68e458fb5b5771285523",
+    "url": "/static/js/main.12201b71.chunk.js"
+  },
+  {
+    "revision": "6cb38723b97afd60a792",
+    "url": "/static/js/runtime-main.195ff595.js"
+  },
+  {
+    "revision": "9799093fee62d4c7b300107e9256c88d",
+    "url": "/static/media/ASML Goodies.9799093f.jpeg"
+  },
+  {
+    "revision": "09ba2126dbb2b6de342f7469c41072e2",
+    "url": "/static/media/ASML ID.09ba2126.jpg"
+  },
+  {
+    "revision": "4c24403399d0cf27dfe6a74a45a1f454",
+    "url": "/static/media/ASMLCard.4c244033.jpeg"
+  },
+  {
+    "revision": "a7c69605086ac6a044b3440552c0cf28",
+    "url": "/static/media/ASMLPosterAll.a7c69605.jpeg"
+  },
+  {
+    "revision": "ef7abce0e5155240ed69379e29e07bd5",
+    "url": "/static/media/CPS-IOT Week Badge.ef7abce0.jpeg"
+  },
+  {
+    "revision": "13eb5838acf21fbbf0cad492c27b1720",
+    "url": "/static/media/CPS-IOT Week Talk.13eb5838.jpeg"
+  },
+  {
+    "revision": "a0bee234595f3aa26da4f1e9b91afa34",
+    "url": "/static/media/GEECSANTS Poster 2025.a0bee234.pdf"
+  },
+  {
+    "revision": "6183c4d9b7251e65c5214843df79cc27",
+    "url": "/static/media/Me Next to GEECSANTS Poster 2025.6183c4d9.jpeg"
+  },
+  {
+    "revision": "8ce6bad055e4d52f1a0a818b11c76548",
+    "url": "/static/media/Prelim.8ce6bad0.pdf"
+  },
+  {
+    "revision": "df7297dc49171bd233ed73615ab39848",
+    "url": "/static/media/Profile Pic.df7297dc.jpg"
+  },
+  {
+    "revision": "f71f52a507df3c16a8f2f301215ab04b",
+    "url": "/static/media/RiwaKaram_Resume.f71f52a5.pdf"
+  },
+  {
+    "revision": "140ef8113b7a9b611dee744ce99ab394",
+    "url": "/static/media/SSCW2025Dinner.140ef811.JPG"
+  },
+  {
+    "revision": "973c9136edacc8d2f2e1517f390e877e",
+    "url": "/static/media/SSCWGroup2025.973c9136.jpg"
+  },
+  {
+    "revision": "ff5b4c1821cfd44435c2384d2f4db7f4",
+    "url": "/static/media/SSCWMe_1_2025.ff5b4c18.jpg"
+  },
+  {
+    "revision": "0a9a514daf9c4c487e5f6e1a65b4bcc4",
+    "url": "/static/media/SSCWMe_2_2025.0a9a514d.jpg"
+  },
+  {
+    "revision": "49bcf5d663ec3791bd2e9af750f7832b",
+    "url": "/static/media/favicon.49bcf5d6.ico"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9dda075319a4ce192b8efe377d5908e.js"
+  "/precache-manifest.00ae17aa0aa8f6ac270007120fa6ce4c.js"
 );
 
 self.addEventListener('message', (event) => {
