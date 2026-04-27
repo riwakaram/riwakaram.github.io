0e192dfad18568c13010525760abd7c46bf0df41
Riwa <riwa.karam@gmail.com>
2026-04-27 01:58:26 -0700

    Updates

--- precache-manifest.54ce90c18e78e6e38c4f224952d2ab88.js
+++ precache-manifest.54ce90c18e78e6e38c4f224952d2ab88.js
@@ -1,35 +1,63 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68b93cd7c29d48463a657ea8743143d6",
+    "revision": "031df78300e72f66b3cb5a9a669d47f4",
     "url": "/index.html"
   },
   {
-    "revision": "f83f1edf1db5fec1e6bf",
+    "revision": "bc7a09def7b793dbfb73",
     "url": "/static/css/2.d49569c5.chunk.css"
   },
   {
-    "revision": "8bb753e692529a2d77f4",
-    "url": "/static/css/main.81dc64b2.chunk.css"
+    "revision": "d32e62c00cef69097a0b",
+    "url": "/static/css/main.589b93c0.chunk.css"
   },
   {
-    "revision": "f83f1edf1db5fec1e6bf",
-    "url": "/static/js/2.52142e12.chunk.js"
+    "revision": "bc7a09def7b793dbfb73",
+    "url": "/static/js/2.b109db1b.chunk.js"
   },
   {
     "revision": "90455f630391e49cb4ec34b7495d1995",
-    "url": "/static/js/2.52142e12.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b109db1b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d1bcd37db3235cc09904",
-    "url": "/static/js/3.b6cdd371.chunk.js"
+    "revision": "a1eeff12c0156a19d182",
+    "url": "/static/js/3.58b486af.chunk.js"
   },
   {
-    "revision": "8bb753e692529a2d77f4",
-    "url": "/static/js/main.e7e6248c.chunk.js"
+    "revision": "d32e62c00cef69097a0b",
+    "url": "/static/js/main.0539901b.chunk.js"
   },
   {
-    "revision": "49c4e90a8de033fb2d4f",
-    "url": "/static/js/runtime-main.4a4e2148.js"
+    "revision": "7faa884bb92dcd19ef12",
+    "url": "/static/js/runtime-main.59ff33cd.js"
+  },
+  {
+    "revision": "70b2b18437bfc62d44055f1d37f5d2b0",
+    "url": "/static/media/01_Fall2020_Honor's_List_Diploma.70b2b184.png"
+  },
+  {
+    "revision": "7c0d1aab0470d06f04b27ae020a7d9d9",
+    "url": "/static/media/02_Spring2021_Honor's_List_Diploma.7c0d1aab.png"
+  },
+  {
+    "revision": "cdb04c0d25d91d96f8f5d1171cbb4561",
+    "url": "/static/media/03_Fall2021_Honor's_List_Diploma.cdb04c0d.jpg"
+  },
+  {
+    "revision": "e87ff8afd72f15b0485803b2ab20a518",
+    "url": "/static/media/04_Spring2022_Honor's_List_Diploma.e87ff8af.png"
+  },
+  {
+    "revision": "6f7b524929492c598f1f94d3507b6f9e",
+    "url": "/static/media/05_Fall2022_Honor's_List_Diploma.6f7b5249.png"
+  },
+  {
+    "revision": "0a4b839d50d4ef17795961aed561c329",
+    "url": "/static/media/06_Spring2023_Honor's_List_Diploma.0a4b839d.png"
+  },
+  {
+    "revision": "7d4aa73d0ce62f937c4fa008ac90b441",
+    "url": "/static/media/ASME Certificate of Participation - Introduction to Simulink.7d4aa73d.jpg"
   },
   {
     "revision": "9799093fee62d4c7b300107e9256c88d",
@@ -38,10 +66,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "69ebf71eeeda8f5fee0e7739816a2dbb",
     "url": "/static/media/ASML2025.69ebf71e.jpeg"
-  },
-  {
-    "revision": "4c24403399d0cf27dfe6a74a45a1f454",
-    "url": "/static/media/ASMLCard.4c244033.jpeg"
   },
   {
     "revision": "a7c69605086ac6a044b3440552c0cf28",
@@ -76,12 +100,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/CPS-IOT Week Talk.13eb5838.jpeg"
   },
   {
+    "revision": "42f32f401fd20516b63b3eeb414a00cb",
+    "url": "/static/media/Faculty Award for Academic Excellence.42f32f40.jpeg"
+  },
+  {
     "revision": "a0bee234595f3aa26da4f1e9b91afa34",
     "url": "/static/media/GEECSANTS Poster 2025.a0bee234.pdf"
   },
   {
     "revision": "58b24080e2f029a70f8a29999d405355",
     "url": "/static/media/Graduation Pic.58b24080.jpg"
+  },
+  {
+    "revision": "288bc616083c98b47179c2c786d75748",
+    "url": "/static/media/Grand Tech 2023 - Certificate of Attendance.288bc616.pdf"
+  },
+  {
+    "revision": "203be072a37b8b143b51926c8e3be511",
+    "url": "/static/media/IEEE Xtreme 2022 Certificate of Participation.203be072.pdf"
+  },
+  {
+    "revision": "83b6e5ef6c6a83da43d9ff827806d9a4",
+    "url": "/static/media/Lebanon AI & IoT Challenge - First Place Winner.83b6e5ef.pdf"
+  },
+  {
+    "revision": "ed917bccbe93e9f4e36032c59df358a7",
+    "url": "/static/media/MATLAB Onramp Course Completion Certificate.ed917bcc.pdf"
   },
   {
     "revision": "be343bad288aa1e56162064243cbb674",
@@ -96,12 +140,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/MeinCDC.7dceb6bf.jpeg"
   },
   {
+    "revision": "517caa825a036e1a7af68df1bed97bc8",
+    "url": "/static/media/Mentor Collective Mentorship.517caa82.pdf"
+  },
+  {
     "revision": "8ce6bad055e4d52f1a0a818b11c76548",
     "url": "/static/media/Prelim.8ce6bad0.pdf"
   },
   {
     "revision": "df7297dc49171bd233ed73615ab39848",
     "url": "/static/media/Profile Pic.df7297dc.jpg"
+  },
+  {
+    "revision": "ad97382bab56f5500439ad240f43e70d",
+    "url": "/static/media/Riwa Karam - 2026-03-26.ad97382b.pdf"
   },
   {
     "revision": "a5c0ba0ab3c555efaa2e642f352efba8",
@@ -128,8 +180,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/SSCWMe_2_2025.0a9a514d.jpg"
   },
   {
+    "revision": "d4b94afb4bc581a1429dfc7528840378",
+    "url": "/static/media/Simulink Onramp Course Completion Certificate.d4b94afb.pdf"
+  },
+  {
     "revision": "a07a0f8effab34f82278d7ff8790a246",
     "url": "/static/media/Survey.a07a0f8e.pdf"
+  },
+  {
+    "revision": "6516fd15faecb69159aeb276dbf4eae6",
+    "url": "/static/media/ZAKA AI Bootcamp.6516fd15.pdf"
   },
   {
     "revision": "49bcf5d663ec3791bd2e9af750f7832b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77a08eeb81682f763288f12281b480d5.js"
+  "/precache-manifest.54ce90c18e78e6e38c4f224952d2ab88.js"
 );
 
 self.addEventListener('message', (event) => {
